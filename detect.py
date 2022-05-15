46f2b108cb374c498308da59323b173ad42914d2
VaibhavBarawkar <99263906+VaibhavBarawkar@users.noreply.github.com>
2022-05-15 16:55:01 +0530

    Add files via upload
    importing modules and loading dataset and capturing the video  for processing

---
+++ detect.py
@@ -0,0 +1,27 @@
+import cv2
+import numpy as np
+import os
+import imutils
+from tensorflow.keras.models import load_model
+//
+
+
+os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
+
+net = cv2.dnn.readNet("yolov3-custom_7000.weights", "yolov3-custom.cfg")
+net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
+
+
+model = load_model('helmet-nonhelmet_cnn.h5')
+print('model loaded!!!')
+
+cap = cv2.VideoCapture('video.mp4')
+COLORS = [(0,255,0),(0,0,255)]
+
+layer_names = net.getLayerNames()
+output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
+ 
+
+fourcc = cv2.VideoWriter_fourcc(*"XVID")
+writer = cv2.VideoWriter('output.avi', fourcc, 5,(888,500))
