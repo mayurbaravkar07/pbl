1d5130ccb7e1e914f3f82b0b761963b778cda01e
SAKSHEEARUDE <100356607+SAKSHEEARUDE@users.noreply.github.com>
2022-05-16 15:08:56 +0530

    Update detect.py
    saving changes

--- detect.py
+++ detect.py
@@ -60,7 +60,9 @@ for out in outs:
             scores = detection[5:]
             class_id = np.argmax(scores)
             confidence = scores[class_id]
-
+if confidence > 0.3:
+                center_x = int(detection[0] * width)
+                center_y = int(detection[1] * height)
 
 
 
